813882823b1aaea64d79f77d6e3074dda55b39e2
stephane <stephane.eicher@he-arc.ch>
2014-10-31 08:53:18 +0100

    Ajout du formulaire et ajout de l'utilisateur dans la DB

--- app/controllers/RegisterController.php
+++ app/controllers/RegisterController.php
@@ -1,49 +1,59 @@
-<?php 
-
+<?php
 namespace EventCal\Controllers;
 
-use EventCal\Models;
+use EventCal\Models\User;
+use EventCal\Models\Society;
+use EventCal\Models\Locality;
 
-class RegisterController extends BaseController{
-	
-	public function getIndex(){
-		return Redirect::action('RegisterController@getUser');
+class RegisterController extends BaseController {
+
+	public function getIndex()
+	{
+		return \Redirect::action('EventCal\Controllers\RegisterController@getUser');
 	}
-	
-	public function getUser(){
-		return \View::make('register.user');
-	}
-	
-	public function getSociety(){
-		return \View::make('register.society');
-	}
-	
-	public function getConfirm(){
-		return \View::make('register.confirm');
-	}
-	
-	public function postUser(){
-		$validator = User::validate(Input::all());
+
+	public function getUser()
+	{
+		$locality = Locality::orderBy('code')->get();
 		
-		if ($validator->fails()) {
-			return Redirect::back()->withErrors($validator)->withInput();
-		}else{
-			return Redirect::action('RegisterController@getSociety')->withInput();
+		$local = array();
+		
+		foreach ($locality as $l)
+		{
+			$local[$l->id] = $l->codeCity();
+					
 		}
+			
+			
+
+		return \View::make('register.user')->with('city',$local);
 	}
-	
-	public function postSociety(){
+
+	public function postUser()
+	{
+		$input = \Input::all();
+		$validatorUser = User::validate($input);
+		$validatorSociety = Society::validate($input);
 		
-		if (false){
-			return Redirect::back()->withInput();
-			//TODO
-		}else {
-			return Redirect::action('');
+		if ($validatorUser->fails() || $validatorSociety->fails())
+		{
+			$notValid = $validatorUser->messages()->merge($validatorSociety->messages());
+			return \Redirect::back()->withErrors($notValid)->withInput();
 		}
 		
-	}
-	
-	public function postConfirm(){
+		$user = new User();
+		$user->fill($input);
+		$user->is_admin = 0;
+		$user->is_actif = 0;
+		$user->save();
+		
+		$society = new Society();
+		$society->fill($input);
+		$society->user_id = $user->id;
+		$society->is_public = 1;
+		$society->save();
+		
+		return \Redirect::to('/');
 		
 	}
 }
--- app/views/register/user.blade.php
+++ app/views/register/user.blade.php
@@ -6,7 +6,10 @@
 	<p>{{$error}}<p>
 	
 @endforeach
-    {{ Form::open(array('action' => 'RegisterController@postUser','method' => 'post')) }}
+    {{ Form::open(array('action' => 'EventCal\Controllers\RegisterController@postUser','method' => 'post')) }}
+    
+    <fieldset>
+    <legend>Information personnel:</legend>
         {{ Form::label('last_name', 'Nom : ') }}
         {{ Form::text('last_name') }}
         {{ Form::label('first_name', 'Prénom : ') }}
@@ -17,6 +20,27 @@
         {{ Form::password('password') }}
         {{ Form::label('password_confirm', 'Confirmer : ') }}
         {{ Form::password('password_confirm') }}
-        {{ Form::submit('Suivant') }}
+	</fieldset>
+	
+	<fieldset>
+    <legend>Information société:</legend>
+        {{ Form::label('name', 'Nom : ') }}
+        {{ Form::text('name') }}
+        {{ Form::label('logo', 'Votre logo : ') }}
+        {{ Form::file('logo') }}
+        {{ Form::label('description', 'Description : ') }}
+        {{ Form::text('description') }}
+        {{ Form::label('website', 'Site internet: ') }}
+        {{ Form::text('website') }}
+        {{ Form::label('locality_id', 'Localité : ') }}
+        {{ Form::select('locality_id', $city) }}
+        {{ Form::label('address', 'Adresse de votre société : ') }}
+        {{ Form::text('address') }}
+        {{ Form::label('telephone', 'Numero de téléphone : ') }}
+        {{ Form::number('telephone') }}
+        {{ Form::label('is_public', 'Voulez que vos données soient publiques ? ') }}
+        {{ Form::checkbox('is_public') }}
+    </fieldset>
+        {{ Form::submit('Enregistrer') }}
 	{{ Form::close() }}
 @stop
