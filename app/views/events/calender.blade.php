5711a653967f6eaa747d9a4f9b0fd6ad2b7bfc66
rossmat <mathieu.rosser@he-arc.ch>
2014-11-18 14:40:58 +0100

    Listing des events ameliores + legende des categories

--- app/views/events/calender.blade.php
+++ app/views/events/calender.blade.php
@@ -1,28 +1,55 @@
 @extends('layout')
 
 @section('contenu')
-	
-	@if (Auth::check())
-		{{Button::normal('Créer nouveau événement')->asLinkTo('event/create')}}
-	@endif
-	
-	@foreach($weekEvents as $day => $events)
-		<p>{{$day}} :</p>
-		
-		@foreach($events as $event)	
-    		<p>{{$event->getHour()}}</p>
-    		<p> Nom de l'event	: {{$event->name}}</p>
-    		<p> Catégorie		: {{$event->category->name}}</p>
-    		<p> organisé par 	: {{$event->society->name}}</p>
-    		<p> Description		: {{$event->description}}</p>
-    		<p> Organisé 		: {{$event->society->name}}</p>
-    		<p> Adresse 		: {{$event->address}}</p>
-    		
-    		<p>{{Button::normal("Voir l'événements")->asLinkTo('event/'.$event->id)}}</p>
-    		<br>
-    		
-    	@endforeach 
-    	
-	@endforeach  
-	
+			
+	<div class="row">
+        <div class="col-md-9">
+        	<table>
+			@foreach($weekEvents as $day => $events)
+				<tr>
+					<td>{{$day}} :</td>
+					
+					<td>
+					@foreach($events as $event)		
+						<?php $color = $event->category->color; # the absolute devil, we need LESS here ?>
+						
+						<a href="{{ url('event/'.$event->id) }}" class="btn" style="background-color: {{ $color }} !important; background-repeat: repeat-x;
+								  background-image: linear-gradient({{ $color }}, {{ $color }});
+								  border-color: {{ $color }};
+								  color: white !important;">
+								{{{ $event->name }}}
+								
+								@if ($event->getHour())
+									{{{ ' (' . $event->getHour() . ')' }}}
+								@endif
+						</a>
+			    	@endforeach 
+			    	</td>
+		    	</tr>
+			@endforeach  
+			</table>
+			
+			@if (Auth::check())
+				{{Button::normal('Créer nouveau événement')->asLinkTo(url('event/create'))}}
+			@endif
+			
+        </div>
+        
+        <div class="col-md-3">
+        	<h4>
+			@foreach ($categories as $cat)
+				<?php $color = $cat->color; # the absolute devil, we need LESS here ?>
+				<p>
+				<span class="label" style="background-color: {{ $color }} !important; background-repeat: repeat-x;
+								  background-image: linear-gradient({{ $color }}, {{ $color }});
+								  border-color: {{ $color }};
+								  color: white !important;">
+			 	{{{ $cat->name }}}
+			  	</span>
+			  	</p>
+			@endforeach
+			</h4>
+        </div>
+	</div>
+
 @stop
