1886c5ce837d3f78fc201c34f8d326c0852008de
stephane <stephane.eicher@he-arc.ch>
2014-12-03 19:09:07 +0100

    design view category

--- app/views/category/formEdit.blade.php
+++ app/views/category/formEdit.blade.php
@@ -1,15 +1,34 @@
-@extends('layoutAdmin') 
+@extends('layout') 
 
 @section('contenu')
 
-			
+	<div class="row">
+        <div class="col-md-12">
+			<h2>{{{ $cat->id ? "Edition d'une categorie" : "Ajout d'une categorie" }}}</h2>
+        </div>
+    </div>
+
 {{ Form::model($cat,array('action'=> array($action, $cat->id), 'method' => $methode)) }}
 	@include('tools.errors')
-	{{ Form::label('name',"Nom cat") }}
-	{{ Form::text('name') }}
-	{{ Form::label('color',"Nom color") }}
-	{{ Form::text('color') }}
-	{{ Form::submit('Enregistrer') }}
+
+	<div class="row">
+		<div class="col-md-6">
+    		<div class="form-group">  	
+	    		{{ Form::label('name',"Nom cat") }}
+				{{ Form::text('name') }}
+	        </div>
+				
+        	<div class="form-group">  
+        	    {{ Form::label('color',"Nom color") }}
+				{{ Form::text('color') }}      
+        	</div>
+        	
+        	<div class="form-group">
+		        {{ Form::submit('Enregistrer') }}
+        	</div>
+		</div>
+	</div>
+		
 {{ Form::close() }}
 
 @stop
--- app/views/category/listing.blade.php
+++ app/views/category/listing.blade.php
@@ -1,40 +1,52 @@
-@extends('layoutAdmin') 
+@extends('layout') 
 
 @section('contenu')
 
-{{ Button::normal("Nouveau")->asLinkTo(url("admin/category/create")) }}
 
-<div class="row">
-	<div class="col-md-12">
-		<table>
-			<thead>
-				<tr>
-					<th>Nom</th>
-					<th>Action</th>
-				</tr>
-			</thead>
-			
-			<tbody>
-				@foreach ($categories as $cat)
-				<tr>
-					<th>
-						<span class="label event-label" style="background-color: {{{ $cat->color }}};"> 
-						{{{ $cat->name }}}
-						</span>
-					</th>
-					<th>
-						{{ Button::normal("Editer")->asLinkTo(url("admin/category/$cat->id/edit")) }}
-						{{ Form::open(array('action'=> array('EventCal\Controllers\Admin\EventCategoryController@destroy', $cat->id), 'method' => 'delete')) }}
-							{{ Form::submit('Supprimer') }}
-						{{ Form::close() }}
-					</th>
-				</tr>
-				@endforeach
-			</tbody>
-			
-		</table>
+
+	<div class="row">
+        <div class="col-md-8">
+			<h2>{{{ "Administration des catégories" }}}</h2>
+        </div>
+        <div class="col-md-4 btn-containers">
+			{{ Button::normal("Ajouter une catégorie")->asLinkTo(url("admin/category/create")) }}
+        </div>
+    </div>
+    
+    <div class="row">
+        <div class="col-md-10 btn-containers">
+        	<table class="table table-condensed">
+				<thead>
+					<tr>
+						<th>Nom</th>
+						<th>Action</th>
+					</tr>
+				</thead>
+				
+				<tbody>
+					@foreach ($categories as $cat)
+					<tr>
+						<th>
+							<h2>
+								<span class="label event-label" style="background-color: {{{ $cat->color }}};"> 
+								{{{ $cat->name }}}
+								</span>
+							</h2>
+						</th>
+						<th>
+							{{ Button::normal("Editer")->asLinkTo(url("admin/category/$cat->id/edit")) }}
+							{{ Form::open(array('action'=> array('EventCal\Controllers\Admin\EventCategoryController@destroy', $cat->id), 'method' => 'delete')) }}
+								{{ Form::submit('Supprimer') }}
+							{{ Form::close() }}
+						</th>
+					</tr>
+					@endforeach
+				</tbody>
+			</table>
+		</div>
 	</div>
-</div>
+	
+	
 
 @stop
 
